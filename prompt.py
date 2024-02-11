b5d7ea87d7751fb19cc63ee25ad542b4e6524e19
davidlacho <david.lacho@gmail.com>
2024-02-10 23:44:42 -0600

    adds redundant_filter_retriever.py

--- prompt.py
+++ prompt.py
@@ -2,19 +2,19 @@ from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores import Chroma
+
+from redundant_filter_retriever import RedundantFilterRetriever
 
 load_dotenv()
 
+chat = ChatOpenAI()
 embeddings = OpenAIEmbeddings()
 db = Chroma(persist_directory="emb", embedding_function=embeddings)
-
-retriever = db.as_retriever()
-
-chat = ChatOpenAI()
+retriever = RedundantFilterRetriever(chroma=db, embeddings=embeddings)
 
 chain = RetrievalQA.from_chain_type(llm=chat, retriever=retriever, chain_type="stuff")
 
-result = chain.run("What is an interesting fact about the English language")
+result = chain.run("What is an interesting fact about the English language?")
 
 print(result)
